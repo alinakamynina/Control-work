fc2a7b9256ff4f94f8d90adb58aedc031a9bfbf9
alina <alinakaminina@yandex.ru>
2022-08-15 09:29:22 +0300

    Файл с котрольной работой

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿string[] array = new string[] { "hello", "2", "world", ":-)" };
+
+string[] array2 = new string[array.Length];
+
+for (int i = 0; i < array.Length; i++)
+{
+    string elements = "";
+
+    for (int j = 0; j < array[i].Length; j++)
+    {
+        elements += array[i][j];
+        array2[i] = elements;
+        if (elements.Length == 3)
+        {
+            break;
+        }
+    }
+}
+Console.WriteLine(string.Join(", ", array2));
